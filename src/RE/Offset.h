27d201ce60dde8f64ab05f6f6eb7b126ac3e8ed5
Parapets <62597298+Exit-9B@users.noreply.github.com>
2024-10-22 20:41:00 +0100

    Modify enchantment creation for staves (#37)

--- src/RE/Offset.h
+++ src/RE/Offset.h
@@ -4,6 +4,11 @@ namespace RE
 {
 	namespace Offset
 	{
+		namespace MagicItemCreationHelpers
+		{
+			constexpr auto CreateNewEnchantment = REL::ID(36178);
+		}
+
 		namespace TESObjectREFR
 		{
 			constexpr auto ActivateCraftingWorkbench = REL::ID(52941);
