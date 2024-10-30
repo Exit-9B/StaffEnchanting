eb3efef782f0d6107d3b3812aae82d21a0cf8711
Parapets <southwest85b@gmail.com>
2024-10-30 23:14:45 +0000

    Set up FOMOD installer

---
+++ fomod.cmake
@@ -0,0 +1,59 @@
+install(
+	FILES
+		"${DATA_DIR}/fomod/info.xml"
+		"${DATA_DIR}/fomod/ModuleConfig.xml"
+	DESTINATION "fomod"
+	COMPONENT fomod
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"${DATA_DIR}/fomod/Images/SkyrimSE.jpg"
+		"${DATA_DIR}/fomod/Images/SkyUIRegular.jpg"
+		"${DATA_DIR}/fomod/Images/SorcererPatch.jpg"
+		"${DATA_DIR}/fomod/Images/StaffEnchanting.jpg"
+	DESTINATION "fomod/Images"
+	COMPONENT fomod
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"$<TARGET_FILE:${PROJECT_NAME}>"
+		"$<TARGET_PDB_FILE:${PROJECT_NAME}>"
+	DESTINATION "Core/SkyrimSE/SKSE/Plugins"
+	COMPONENT fomod
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"${DATA_DIR}/Common/StaffEnchanting.esp"
+	DESTINATION "Common"
+	COMPONENT fomod
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES ${TRANSLATION_FILES}
+	DESTINATION "Common/Interface/Translations"
+	COMPONENT fomod
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"${DATA_DIR}/Menu/SkyUI/Interface/StaffCraftingMenu.swf"
+	DESTINATION "Menu/SkyUI/Interface"
+	COMPONENT fomod
+	EXCLUDE_FROM_ALL
+)
+
+install(
+	FILES
+		"${DATA_DIR}/Patches/Sorcerer/StaffEnchantingSorcererPatch.esp"
+	DESTINATION "Patches/Sorcerer"
+	COMPONENT fomod
+	EXCLUDE_FROM_ALL
+)
