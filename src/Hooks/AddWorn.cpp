fd0e81a91ea7c40fb3238d68c577a9485bef996d
Parapets <southwest85b@gmail.com>
2026-04-24 19:13:45 +0100

    add missing pattern check

--- src/Hooks/AddWorn.cpp
+++ src/Hooks/AddWorn.cpp
@@ -6,6 +6,7 @@ namespace Hooks
 	void AddWorn::Install()
 	{
 		auto hook = util::GameAddress(RE::Offset::TESObjectWEAP::IsThrownWeapon);
+		REL::make_pattern<"0F B6 91 9D 01 00 00">().match_or_fail(hook.address());
 		REL::safe_fill(hook.address(), REL::NOP, 0x18);
 	}
 }
